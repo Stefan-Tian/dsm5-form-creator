9adb3afe8b2f12001b990ed46232e9c4d47f0505
Sheng Hong Tian <stefan55831@gmail.com>
2018-06-06 14:43:04 +0800

    Randomize the presentation sequence

--- Code.js
+++ Code.js
@@ -1,15 +1,15 @@
-symptoms = [
-  "對事情或活動有過度的焦慮和擔憂",
-  "睡眠困擾",
-  "肌肉緊繃",
-  "易怒",
-  "腦中一片空白",
-  "注意力不集中",
-  "容易疲勞",
-  "心情不定",
-  "感覺緊張",
-  "坐立不安",
-  "個案認為難以控制面臨的擔憂"
+var symptoms = [
+  '"對事情或活動有過度的焦慮和擔憂"',
+  '"睡眠困擾"',
+  '"肌肉緊繃"',
+  '"易怒"',
+  '"腦中一片空白"',
+  '"注意力不集中"',
+  '"容易疲勞"',
+  '"心情不定"',
+  '"感覺緊張"',
+  '"坐立不安"',
+  '"個案認為難以控制面臨的擔憂"'
 ];
 
 function getCombination(symptoms) {
@@ -29,6 +29,18 @@ function getCombination(symptoms) {
 
 var set = getCombination(symptoms);
 
+/// random the set!!
+var newSet = [];
+for (var i = 0; i < set.length; i++) {
+  var randomNum = Math.floor(Math.random() * set.length);
+  while (set[randomNum] === undefined) {
+    randomNum = Math.floor(Math.random() * set.length);
+  }
+  newSet.push(set[randomNum]);
+
+  set[randomNum] = undefined;
+}
+
 var form = FormApp.create("GAD問卷");
 
 form.setDescription(
@@ -37,40 +49,45 @@ form.setDescription(
 
 var pages = [];
 var items = [];
-var pageLength = set.length * 2;
-for (var i = 0; i < pageLength; i++) {
-  var page = form.addPageBreakItem();
-  pages.push(page);
-  var item = form.addMultipleChoiceItem();
-  items.push(item);
+for (var i = 0; i < newSet.length; i++) {
+  var page1 = form.addPageBreakItem();
+  pages.push(page1);
+  var item1 = form.addMultipleChoiceItem();
+  items.push(item1);
+  var page2 = form.addPageBreakItem();
+  pages.push(page2);
+  var item2 = form.addScaleItem();
+  items.push(item2);
 }
 
 var j = 0; // set index
 var k = 0; // followup index
+var pageLength = newSet.length * 2;
 for (var i = 0; i < pageLength; i++) {
   if (i == 108) {
     items[i]
-      .setTitle(set[54]) // question 2 start from the last
+      .setTitle(newSet[54]) // question 2 start from the last
       .setChoices([
         items[i].createChoice("無關", FormApp.PageNavigationType.SUBMIT),
-        items[i].createChoice("左 -> 右", FormApp.PageNavigationType.CONTINUE),
-        items[i].createChoice("左 <- 右", FormApp.PageNavigationType.CONTINUE),
-        items[i].createChoice("左 <-> 右", FormApp.PageNavigationType.CONTINUE)
+        items[i].createChoice("左 --> 右", FormApp.PageNavigationType.CONTINUE),
+        items[i].createChoice("左 <-- 右", FormApp.PageNavigationType.CONTINUE),
+        items[i].createChoice("左 <--> 右", FormApp.PageNavigationType.CONTINUE)
       ]);
   } else if (i == 0 || i % 2 == 0) {
     items[i]
-      .setTitle(set[j])
+      .setTitle(newSet[j])
       .setChoices([
         items[i].createChoice("無關", pages[i + 2]),
-        items[i].createChoice("左 -> 右", pages[i + 1]),
-        items[i].createChoice("左 <- 右", pages[i + 1]),
-        items[i].createChoice("左 <-> 右", pages[i + 1])
+        items[i].createChoice("左 --> 右", pages[i + 1]),
+        items[i].createChoice("左 <-- 右", pages[i + 1]),
+        items[i].createChoice("左 <--> 右", pages[i + 1])
       ]);
     j = j + 1;
   } else {
     items[i]
-      .setTitle(set[k] + " 的因果或共同關係強度")
-      .setChoiceValues(["強", "中", "弱"]);
+      .setTitle(newSet[k] + " 的因果或共同關係強度")
+      .setBounds(1, 3)
+      .setLabels("弱", "強");
     k = k + 1;
   }
 }
--- utils/strings.js
+++ utils/strings.js
@@ -1,5 +1,42 @@
-// var world = "世界";
+// var symptoms = [
+//   '"對事情或活動有過度的焦慮和擔憂"',
+//   '"睡眠困擾"',
+//   '"肌肉緊繃"',
+//   '"易怒"',
+//   '"腦中一片空白"',
+//   '"注意力不集中"',
+//   '"容易疲勞"',
+//   '"心情不定"',
+//   '"感覺緊張"',
+//   '"坐立不安"',
+//   '"個案認為難以控制面臨的擔憂"'
+// ];
 
-// function hello() {
-//   Logger.log("Hello " + world);
+// function getCombination(symptoms) {
+//   var result = [];
+//   function func(symptoms) {
+//     var first = symptoms.shift();
+//     for (var i = 0; i < symptoms.length; i++) {
+//       var comb = first + " vs " + symptoms[i];
+//       result.push(comb);
+//     }
+//   }
+//   while (symptoms && symptoms.length > 1) {
+//     func(symptoms);
+//   }
+//   return result;
+// }
+
+// var set = getCombination(symptoms);
+
+// // random the set!!
+// var newSet = [];
+// for (var i = 0; i < set.length; i++) {
+//   var randomNum = Math.floor(Math.random() * set.length);
+//   while (set[randomNum] === undefined) {
+//     randomNum = Math.floor(Math.random() * set.length);
+//   }
+//   newSet.push(set[randomNum]);
+
+//   set[randomNum] = undefined;
 // }
